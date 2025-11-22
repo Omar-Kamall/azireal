22c3e4febea25ede6b71592b8fe99df597b0315b
Omar-Kamal <omar.k.0000000@gmail.com>
2025-11-22 19:15:54 +0200

    Update main.js

--- main.js
+++ main.js
@@ -133,7 +133,7 @@ document.addEventListener("DOMContentLoaded", function () {
         btn.disabled = true;
 
         // https://azrieal-server.vercel.app/api/submit/
-        fetch("/api/sendOrder", {
+        fetch("https://azrieal.vercel.app/api/sendOrder/", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(orderData),
@@ -167,4 +167,5 @@ document.addEventListener("DOMContentLoaded", function () {
             });
     });
 });
+
 
